8dbe3657eaf7e239ee588310b451cf6873c9484b
joohyeson <juhye.son990902@gmail.com>
2021-07-23 20:17:31 +0900

    Part 2 - 2강 평화로운 쇼핑몰 레이아웃 디자인시간

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,11 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-		<button type="button" class="btn btn-primary">Primary</button>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Shoe</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="jumbotron">
+        <br />
+        <h1>20% Seaon Off</h1>
+        <br />
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <br />
+        <p>
+          <Button variant="dark">Learn More</Button>{' '}
+        </p>
+        <br />
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+        </div>
+      </div>
+
+
+
     </div>
+
+
   );
 }
 
